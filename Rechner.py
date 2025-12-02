dc75ac02ed2ec387dd55a889e6a4b05dcf131274
AmirHz <amir.py.hosseinzade@gmail.com>
2025-12-02 19:09:17 +0100

    Frame-Anzeige

--- Rechner.py
+++ Rechner.py
@@ -18,3 +18,38 @@ class Calculator:
         self.display_value = StringVar()
         self.display_value.set("0")
         self.display_value.trace('w' , self.update_display_size)
+        
+        self.create_sections()
+        
+    # Frames erstellen
+    def create_sections(self):
+        self.display_frame  = Frame(self.window, width= 380, height=75, bg=self.farbe)
+        self.display_frame .pack(side=TOP ,pady=15 , fill='both' )
+
+        self.buttons_frame  = Frame(self.window , width=380, height=325, bg=self.farbe)
+        self.buttons_frame .pack(padx= 5 ,fill="both" , pady=10 , expand=True)
+        self.create_display()
+        self.create_buttons()
+
+    # Anzeige erstellen
+    def create_display(self):
+        self.display_label = Label(self.display_frame, font=("Arial", 27, 'bold'),
+                                   textvariable=self.display_value, bg=self.farbe,
+                                    fg="white", anchor="e", padx=10,
+                                    relief="flat")
+        self.display_label.pack(ipadx=20, ipady=10, fill="both", expand=True)
+
+    # Schriftgröße dynamisch anpassen
+    def update_display_size(self , *args):
+        current_text = self.display_value.get()
+        text_length = len(current_text)
+
+        if text_length <= 16:
+            font_size = 27
+        elif text_length <= 20:
+            font_size = 24
+        elif text_length <= 24:
+            font_size = 20
+        else:
+            font_size = 17
+        self.display_label.config(font=("Arial", font_size, "bold"))
